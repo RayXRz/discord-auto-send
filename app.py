4468191d66c8576fde8cf8eaa94e39aeefe5cd98
RayXRz <112157153+RayXRz@users.noreply.github.com>
2025-10-19 17:38:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ ADMIN_KEY = os.environ.get("ADMIN_KEY", "09062008DhafinARz!")
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'replace_this_secret_in_production'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:SrRRkhFqeTQxWfPNpoSpbUSNUjeZJvwv@postgres.railway.internal:5432/railway"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
